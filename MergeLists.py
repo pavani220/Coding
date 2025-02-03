24da9aa813ab75e970437b22db91e4343f625a4b
pavani2207 <141611434+pavani2207@users.noreply.github.com>
2025-02-03 12:17:10 +0530

    updated

--- MergeLists.py
+++ MergeLists.py
@@ -9,11 +9,19 @@
 #a=[1,2,3]
 #b=[4,5,6]
 #c=list(zip(a,b))
+#output:[(1,4),(2,5),(3,6)]
+
+#3.output as :[(1,2,3),(4,5,6)]
+a=[1,2,3]
+b=[4,5,6]
+c=[(tuple(a)),(tuple(b))]
+print(c)
+#4.Without Zip
+#a=[1,2,3]
+#b=[4,5,6]
+#c=[]
+#for i in range(len(a)): 
+#    c.append((a[i],b[i]))
 #print(c)
 #output:[(1,4),(2,5),(3,6)]
 
-a = [1, 2, 3]
-b = [4, 5, 6]
-
-c = list(zip([a, b]))  # Transpose the lists
-print(c)
