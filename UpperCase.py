8f98953622681debf4e8af0173301d301483388d
pavani2207 <141611434+pavani2207@users.noreply.github.com>
2025-02-05 14:36:25 +0530

    adding new files

---
+++ UpperCase.py
@@ -0,0 +1,14 @@
+def convert_to_upper(s):
+    result=" "
+    for i in s:
+        if 'a'<=i<='z':
+            result+=chr(ord(i)-32)
+        else:
+            result+=i
+    return result
+        
+a="aAabb"
+print(convert_to_upper(a))
+
+#AAABB
+#use + for lowercase
